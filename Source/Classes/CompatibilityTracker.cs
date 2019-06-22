12bd447c15119843d68465449f6d2643852f505f
KongMD <KongMDSteam@gmail.com>
2019-06-22 15:30:36 -0400

    Log an error when incompatible mod loaded

--- Source/Classes/CompatibilityTracker.cs
+++ Source/Classes/CompatibilityTracker.cs
@@ -7,6 +7,7 @@ namespace LifeSupport
     public static class CompatibilityTracker
     {
         private static bool deathRattleActiveInt = false;
+        private static string[] incompatibleModArr = { "Questionable Ethics" };
 
         public static bool DeathRattleActive
         {
@@ -16,8 +17,35 @@ namespace LifeSupport
             }
         }
 
+        public static string[] IncompatibleMods
+        {
+            get
+            {
+                return incompatibleModArr;
+            }
+            set
+            {
+                incompatibleModArr = value;
+            }
+        }
+
         static CompatibilityTracker()
         {
+            foreach (string s in incompatibleModArr)
+            {
+                if (ModsConfig.ActiveModsInLoadOrder.Any((ModMetaData m) => m.Name == s))
+                {
+                    switch(s)
+                    {
+                        case "Questionable Ethics":
+                            Log.Error("Life Support is incompatible with " + s + ". Download and use Questionable Ethics Enhanced, instead!");
+                            break;
+                        default:  Log.Error("Life Support is incompatible with " + s);
+                            break;
+                    }
+                }
+            }
+
             //Check for Death Rattle Compatiblity.
             if (GenTypes.AllTypes.Any(type => type.FullName == "DeathRattle.DeathRattleBase"))
             {
